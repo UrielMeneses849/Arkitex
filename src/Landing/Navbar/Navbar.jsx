ba07a0ab4d0508d8d2ed32ef53597d315366c71b
uriel <urielmeneses849@gmail.com>
2023-10-05 18:08:42 -0600

    cambios

--- src/Landing/Navbar/Navbar.jsx
+++ src/Landing/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
-import { Grid } from '@mui/material';
+import { Grid, Button } from '@mui/material';
 import './Navbar.css';
 import Anchor from './Anchor/Anchor';
 
 export default function Navbar(){
 return(
-<>
+<div className='navbar'>
  {/*Icono de menu para celulares*/}
- <img src='./assets/Frame.svg' style={{width: '32px'}}/>
-          <h2>ARKITEX</h2>
+ <div className="icono">
+
+ <img src='./assets/Frame.svg' style={{width: '72px'}}/>
+          <h2 className='Arkitex'>ARKITEX</h2>
+
+ </div>
  <Grid justifyContent={"center"} display={{xs:"flex",md:"none"}}>
         
         </Grid>
@@ -19,6 +23,8 @@ return(
           <Anchor textoAnchor='Testimonios'></Anchor>
           <Anchor textoAnchor='Acerca de Nosotros'></Anchor>
           <Anchor textoAnchor='Contacto'></Anchor>
-        </Grid>
-</>
+  </Grid>
+  <Button variant='contained'> Iniciar Sesion </Button>
+      
+</div>
 )}
