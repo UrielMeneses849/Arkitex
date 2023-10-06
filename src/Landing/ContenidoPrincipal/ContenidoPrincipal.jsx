9ba9e47bf7b0c7901fee43478a27ac6719ac51c6
uriel <urielmeneses849@gmail.com>
2023-10-05 18:53:25 -0600

    cambios 3

--- src/Landing/ContenidoPrincipal/ContenidoPrincipal.jsx
+++ src/Landing/ContenidoPrincipal/ContenidoPrincipal.jsx
@@ -2,13 +2,13 @@
 import "./ContenidoPrincipal.css"
 export default function ContenidoPrincipal(){
 return(
-<>
-<>
+<div className="contenidoPrincipal">
+
             <h1 className='indexH1' style={{color: "#121212"}}>Prueba ARKITEX y...</h1>
             <p className='indexP1'>Encuentra ofertas laborales en obras</p>
             <p className='indexP2'>ó</p>
             <p className='indexP3' style={{color: "#121212"}}>Inicia una remodelación para tu hogar</p>
             <p className='indexP4'>Arkitex, la plataforma diseñada para conectar empleador y trabajador para realizar una remodelación, por favor comparte tu trabajo aqui.</p>
-        </>
-</>
+    
+</div>
 )}
