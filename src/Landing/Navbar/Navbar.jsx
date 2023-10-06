b810679c5d0bf70a7c4affd0468fe0f292c87d3c
uriel <urielmeneses849@gmail.com>
2023-10-05 18:41:30 -0600

    Cambios Uriel

--- src/Landing/Navbar/Navbar.jsx
+++ src/Landing/Navbar/Navbar.jsx
@@ -24,7 +24,11 @@ return(
           <Anchor textoAnchor='Acerca de Nosotros'></Anchor>
           <Anchor textoAnchor='Contacto'></Anchor>
   </Grid>
-  <Button variant='contained'> Iniciar Sesion </Button>
+  <Button variant='contained' sx={{backgroundColor: "#FF9500", width: "300px", height: "60px", borderRadius: "20px", fontWeight: "700",
+  '&:hover':{
+    backgroundColor: "#da7110"
+  }
+  }}> Iniciar Sesion </Button>
       
 </div>
 )}
