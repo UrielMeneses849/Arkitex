d59dd8cb196079fde3bf45a13aff3e2b80e898b4
Luis <sanchezmendozajoseluis9@gmail.com>
2023-10-05 18:15:24 -0600

    Cambios

--- src/Landing/Navbar/Navbar.jsx
+++ src/Landing/Navbar/Navbar.jsx
@@ -5,11 +5,10 @@ import Anchor from './Anchor/Anchor';
 export default function Navbar(){
 return(
 <>
- {/*Icono de menu para celulares*/}
- <img src='./assets/Frame.svg' style={{width: '32px'}}/>
-          <h2>ARKITEX</h2>
- <Grid justifyContent={"center"} display={{xs:"flex",md:"none"}}>
-        
+{/*Icono de menu para celulares*/}
+<img src='./assets/Frame.svg' style={{width: '32px'}}/>
+<h2>ARKITEX</h2>
+<Grid justifyContent={"center"} display={{xs:"flex",md:"none"}}>
         </Grid>
         {/*Enlaces de la barra de navegacion*/}
         <Grid className='navegacion' component={"nav"} maxHeight={{md:"200px"}} display={"flex"}
