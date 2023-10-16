be5a34d653fb8c5cf52899ef5d2043205a08c1ac
uriel <urielmeneses849@gmail.com>
2023-10-16 04:46:51 -0600

    Agregando primera pantalla de login empleador

--- src/RegistroEmpleador/RegistroEmpleador.jsx
+++ src/RegistroEmpleador/RegistroEmpleador.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import Box from '@mui/material/Box';
+import {  Button, Grid, Step, StepLabel, Stepper,} from '@mui/material';
+import frame from '/assets/Group 161.png';
+import { useState } from 'react';
+// Componentes del formulario de registro
+import EscogerTrabajo from './EscogerTrabajo';
 
-function RegistroEmpleador() {
+// --------------------------------------------------------Funcion principal----------------------------------------------------------------------------------------------------------------------
+export default function RegistroTrabajador() {
+//  Estado que almaceba la posicion del stepper
+const [activeStep, setActiveStep] = useState(0);
+// Funcion para avanzar de paso
+const nextStep = () => {
+    if(activeStep < 3) 
+        setActiveStep(currentStep => currentStep + 1);
+}
+// Funcion para retroceder de paso
+const previousStep = () => {
+    if(activeStep !== 0) 
+        setActiveStep(currentStep => currentStep - 1);
+}
+
   return (
-    <div>RegistroEmpleador</div>
+    // Contenedor principal
+    <Box bgcolor={"#fafafa"} component={'main'} padding={'2rem 1rem'} height='100vh' sx={{ boxSizing: 'border-box' }}>
+      <Grid container columns={2} height='100%' bgcolor='#F0F0F0' borderRadius={'30px'}>
+        <Grid item xs={1} height='100%' width='50%'>
+            {/* Imagen izquierda del login */}
+          <img src={frame} alt='frame' style={{ maxHeight: '100%', minWidth: '100%' }}></img>
+        </Grid>
+        <Grid item component={'div'} xs={1} width='50%' height='100%' padding='1rem 0'
+          display='flex' flexDirection='column'>
+          <h1 style={{ textAlign: 'center', fontWeight: '700', fontSize: '40px', margin: '4rem 0 2rem' }}>Registrate como empleador</h1>
+    {/* Textos de ayuda dependiendo del paso en el que este */}
+          {activeStep === 0 && <h4 style={{color: "#888888"}}>Escoge que tipo de trabajo necesitas</h4>}
+          {activeStep === 1 && <h4 style={{color: "#888888"}}>¿Que area se va a trabajar?</h4> }
+          {activeStep === 2 && <h4 style={{color: "#888888"}}>Texto 3</h4> }
+          {activeStep === 3 && <h4 style={{color: "#888888"}}>Texto 4</h4> }  
+
+
+          <Box sx={{ width: '100%', marginTop: '2rem' }} margin='0'>
+           
+          <Stepper activeStep={activeStep}>
+                <Step>
+                    <StepLabel>Elige</StepLabel>
+                </Step>
+                <Step>
+                    <StepLabel>Escoge </StepLabel>
+                </Step>
+                <Step>
+                    <StepLabel>Escoge</StepLabel>
+                </Step>
+                <Step>
+                    <StepLabel>Escoge </StepLabel>
+                </Step>
+          </Stepper>
+            {/* Contenido de cada paso */}
+        <Box margin={"30px auto"} style={{ height: "85%"}}>
+
+          {activeStep == 0 && <EscogerTrabajo />}
+          {activeStep == 1 && <h2>Hola2</h2>}
+          {activeStep == 2 && <h2>Hola3</h2>}
+          {activeStep == 3 && <h2>Hola4</h2>}
+          
+        </Box>
+          <Box display={"flex"} justifyContent={"space-around"} margin={"10px auto 0"}>
+          {/* Botones de pasos */}
+            <Button variant='outlined' onClick={() => previousStep()}> Atras </Button>
+            <Button variant='contained' onClick={() => nextStep()}> {activeStep === 3 ? "Terminar" : "Siguiente"} </Button>
+          </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
-export default RegistroEmpleador
